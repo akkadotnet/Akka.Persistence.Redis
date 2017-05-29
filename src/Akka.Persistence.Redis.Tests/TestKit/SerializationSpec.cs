389de124c3ec1ca87c408e343d34c88dc2025607
alexvaluyskiy <yurisikorsk@gmail.com>
2017-05-29 18:29:55 +0300

    Simplify serialization specs

--- src/Akka.Persistence.Redis.Tests/TestKit/SerializationSpec.cs
+++ src/Akka.Persistence.Redis.Tests/TestKit/SerializationSpec.cs
@@ -1,148 +1,61 @@
-﻿using Akka.Actor;
+﻿using System;
+using System.Collections.Generic;
+using Akka.Actor;
 using Akka.Configuration;
+using Akka.Persistence.TestKit;
+using Akka.TestKit;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Akka.Persistence.Redis.Tests.TestKit
 {
-    public abstract class SerializationSpec : Akka.TestKit.Xunit2.TestKit
+    public abstract class SerializationSpec : PluginSpec
     {
-        #region Actors
-        public class Msg
-        {
-            public Msg(long deliveryId, string message)
-            {
-                DeliveryId = deliveryId;
-                Message = message;
-            }
-
-            public long DeliveryId { get; }
-
-            public string Message { get; }
-        }
-
-        public class Confirm
-        {
-            public Confirm(long deliveryId)
-            {
-                DeliveryId = deliveryId;
-            }
-
-            public long DeliveryId { get; }
-        }
-
-        public class MsgSent
-        {
-            public MsgSent(string message)
-            {
-                Message = message;
-            }
-
-            public string Message { get; }
-        }
-
-        public class MsgConfirmed
-        {
-            public MsgConfirmed(long deliveryId)
-            {
-                DeliveryId = deliveryId;
-            }
-
-            public long DeliveryId { get; }
-        }
-
-        public class SourceActor : AtLeastOnceDeliveryActor
-        {
-            private readonly IActorRef _replyTo;
-            private readonly IActorRef _destionationActor = Context.ActorOf<DestinationActor>();
-
-            public SourceActor(string persistenceId, IActorRef replyTo)
-            {
-                _replyTo = replyTo;
-                PersistenceId = persistenceId;
-            }
-
-            protected override bool ReceiveRecover(object message)
-            {
-                if (message is SnapshotOffer offer && offer.Snapshot is AtLeastOnceDeliverySnapshot atLeast)
-                {
-                    SetDeliverySnapshot(atLeast);
-                }
-                else if (message is RecoveryCompleted)
-                {
-                    _replyTo.Tell("done");
-                }
-
-                return true;
-            }
-
-            protected override bool ReceiveCommand(object message)
-            {
-                switch (message)
-                {
-                    case string str when str.Equals("stop"):
-                        Context.Stop(Self);
-                        return true;
-                    case string str when str.Equals("snap"):
-                        SaveSnapshot(GetDeliverySnapshot());
-                        return true;
-                    case string str:
-                        Persist(new MsgSent(str), msgSent =>
-                            Deliver(_destionationActor.Path, l => new Msg(l, msgSent.Message)));
-                        return true;
-                    case Confirm confirm:
-                        Persist(new MsgConfirmed(confirm.DeliveryId), msgConfirmed =>
-                            ConfirmDelivery(msgConfirmed.DeliveryId));
-                        return true;
-                    default:
-                        return true;
-                }
-            }
-
-            public override string PersistenceId { get; }
-        }
-
-        public class DestinationActor : UntypedActor
-        {
-            protected override void OnReceive(object message)
-            {
-                switch (message)
-                {
-                    case Msg msg when msg.Message.Equals("unconfirmed"):
-                        break;
-                    case Msg msg:
-                        Sender.Tell(new Confirm(msg.DeliveryId), Self);
-                        break;
-                }
-            }
-        }
-        #endregion
-
         protected SerializationSpec(Config config, ITestOutputHelper output) : base(config, null, output)
         {
         }
 
+        protected IActorRef Journal => Extension.JournalFor(null);
+        protected IActorRef SnapshotStore => Extension.SnapshotStoreFor(null);
+
         [Fact]
         public void AtLeastOnceDeliveryActor_should_serialize_AtLeastOnceDeliverySnapshot()
         {
-            var sourceActor = Sys.ActorOf(Props.Create<SourceActor>("SourceActor", TestActor));
-            ExpectMsg("done");
-            sourceActor.Tell("a");
-            sourceActor.Tell("b");
-            sourceActor.Tell("unconfirmed");
-            sourceActor.Tell("snap");
-            sourceActor.Tell("stop");
-            Watch(sourceActor);
-            ExpectTerminated(sourceActor);
+            var probe = CreateTestProbe();
 
-            var sourceActor2 = Sys.ActorOf(Props.Create<SourceActor>("SourceActor", TestActor));
-            ExpectMsg("done");
+            var deliveries = new UnconfirmedDelivery[]
+            {
+                new UnconfirmedDelivery(1, ActorPath.Parse("akka://my-sys/user/service-a/worker1"), "test1"),
+                new UnconfirmedDelivery(2, ActorPath.Parse("akka://my-sys/user/service-a/worker2"), "test2"),
+            };
+            var atLeastOnceDeliverySnapshot = new AtLeastOnceDeliverySnapshot(2L, deliveries);
+
+            var metadata = new SnapshotMetadata(Pid, 2);
+            SnapshotStore.Tell(new SaveSnapshot(metadata, atLeastOnceDeliverySnapshot), probe.Ref);
+            probe.ExpectMsg<SaveSnapshotSuccess>();
+
+            SnapshotStore.Tell(new LoadSnapshot(Pid, SnapshotSelectionCriteria.Latest, long.MaxValue), probe.Ref);
+            probe.ExpectMsg<LoadSnapshotResult>(s => s.Snapshot.Snapshot.Equals(atLeastOnceDeliverySnapshot));
         }
 
-        [Fact(Skip = "Not implemented yet")]
+        [Fact]
         public void PersistenceFSM_should_serialize_StateChangeEvent()
         {
+            var probe = CreateTestProbe();
+            var stateChangeEvent = new Akka.Persistence.Fsm.PersistentFSMBase<string, string, string>.StateChangeEvent("init", TimeSpan.FromSeconds(342));
+            
+            var messages = new List<AtomicWrite>()
+            {
+                new AtomicWrite(new Persistent(stateChangeEvent, 1, Pid))
+            };
 
+            Journal.Tell(new WriteMessages(messages, probe.Ref, ActorInstanceId));
+            probe.ExpectMsg<WriteMessagesSuccessful>();
+            probe.ExpectMsg<WriteMessageSuccess>(m => m.ActorInstanceId == ActorInstanceId && m.Persistent.PersistenceId == Pid);
+
+            Journal.Tell(new ReplayMessages(0, 1, long.MaxValue, Pid, probe.Ref));
+            probe.ExpectMsg<ReplayedMessage>();
+            probe.ExpectMsg<RecoverySuccess>();
         }
 
         [Fact(Skip = "Not implemented yet")]
