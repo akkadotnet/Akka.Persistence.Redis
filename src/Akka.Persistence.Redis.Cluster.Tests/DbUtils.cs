f2cbfe15d50af1abd51c0badf9dd7ab6486a5a39
Gregorius Soedharmo <arkatufus@yahoo.com>
2021-02-10 04:34:28 +0700

    Refactor namespace from Test to Tests

--- src/Akka.Persistence.Redis.Cluster.Tests/DbUtils.cs
+++ src/Akka.Persistence.Redis.Cluster.Tests/DbUtils.cs
@@ -5,10 +5,9 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
-using System.Linq;
 using StackExchange.Redis;
 
-namespace Akka.Persistence.Redis.Cluster.Test
+namespace Akka.Persistence.Redis.Cluster.Tests
 {
     public static class DbUtils
     {
